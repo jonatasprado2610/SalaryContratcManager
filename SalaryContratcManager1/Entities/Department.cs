b355a4f66b11fefc68e27f496f82b9465ad16b81
Jonatas Da Silva Prado <jonatasprado445@gmail.com>
2025-09-18 13:59:43 -0300

    SalaryContractManager

---
+++ SalaryContratcManager1/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SalaryContratcManager1.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ SalaryContratcManager1/Entities/Worker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using SalaryContratcManager1.Entities.Enums;
+using System.Collections.Generic;
+
+
+
+namespace SalaryContratcManager1.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void addContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void removeContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+
+            foreach (HourContract contratc in Contracts)
+            {
+
+                if (contratc.Date.Year == year && contratc.Date.Month == month)
+                {
+                    sum += contratc.TotalValue();
+                }
+
+            }
+            return sum;
+
+        }
+    }
+}
+
